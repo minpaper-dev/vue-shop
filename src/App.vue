2b89445fca3f8a44fe3e9a9d8f90ff93fb54b2d4
minji-yun <minpaper.dev@gmail.com>
2023-07-07 18:22:41 +0900

    [feat] Component 등록

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <Header />
+  <CarouselBanner />
+  <div v-for="item in productList" :key="item.category">
+    <ProductList v-bind:product="item" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <Footer />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script lang="js">
+import axios from "axios";
+
+import Header from "./components/Header.vue";
+import CarouselBanner from "./components/CarouselBanner.vue";
+import Footer from './components/Footer.vue'
+import ProductList from './components/ProductList.vue'
+
+export default {
+  name : 'App',
+  components : {
+    Header, CarouselBanner, Footer, ProductList
+  },
+  data(){
+    return {
+      categoryList : ["electronics", "jewelery", "men's clothing"],
+      productList : []
+    }
+  },
+  methods : {
+    get(category) {
+      axios.get(`https://fakestoreapi.com/products/category/${category}?limit=4`)
+      .then((result)=>{
+        console.log(result.data)
+        this.productList.push({category, data : result.data})
+      })
+    },
+
+  },
+  created()  {
+    for (const category in this.categoryList) {
+      this.get(this.categoryList[category])
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
+
+<style scoped></style>
