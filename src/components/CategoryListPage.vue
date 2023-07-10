018ba385f950e982eee6791dd5aa731f10ae83a5
minji-yun <minpaper.dev@gmail.com>
2023-07-10 21:17:25 +0900

    [feat] CategoryPage 생성

---
+++ src/components/CategoryListPage.vue
@@ -0,0 +1,32 @@
+<script>
+import { watch, ref } from "vue";
+import ProductList from "./ProductList.vue";
+
+export default {
+  components: { ProductList },
+  props: {
+    category: String,
+  },
+  setup(props) {
+    let category = ref(props.category);
+
+    watch(
+      () => props.category,
+      (newValue, oldValue) => {
+        console.log(newValue);
+        category.value = newValue;
+      }
+    );
+
+    return {
+      category,
+    };
+  },
+};
+</script>
+
+<template>
+  <product-list v-bind:category="category"></product-list>
+</template>
+
+<style scoped></style>
