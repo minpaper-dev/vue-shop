a24fe269c58ea3dfeeaabf1106ffeeb275091ad4
minji-yun <minpaper.dev@gmail.com>
2023-07-10 21:17:12 +0900

    [feat] 상품 List, Item Component화

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,105 @@
+<script>
+import { watch, ref } from "vue";
+
+export default {
+  props: {
+    product: Object,
+  },
+  setup(props) {
+    let product = ref(props.product);
+
+    watch(
+      () => props.product,
+      (newValue, oldValue) => {
+        product = newValue;
+      }
+    );
+
+    return {
+      product,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="product-list">
+      <a class="product-item">
+        <figure class="product-card-wrap-image">
+          <img :src="product.image" class="product-card-image" />
+        </figure>
+        <div class="product-card-description">
+          <span>{{ product.title }}</span>
+          <p>${{ product.price }}</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  margin-bottom: 32px;
+  color: #fff;
+}
+.container {
+  max-width: 1360px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  /* position: relative;
+  top: 62px; */
+  padding: 48px 8px 32px;
+  margin: 0 auto;
+}
+.product-card-wrap-image {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+}
+.product-card-image {
+  width: 100px;
+  height: 100px;
+}
+.product-item {
+  max-width: 316px;
+  width: 316px;
+  height: 468px;
+  overflow: hidden;
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+.product-card {
+}
+.product-list {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.product-card-description {
+  width: 100%;
+  min-height: 180px;
+  color: #fff;
+  background-color: #394150;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 32px;
+}
+
+.product-card-description span {
+  margin-bottom: 20px;
+}
+</style>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,92 +1,63 @@
 <script>
+import { onMounted, ref, watch } from "vue";
+import axios from "axios";
+import ProductItem from "./ProductItem.vue";
+
 export default {
-  name: "ProductList",
   props: {
-    product: Object,
+    category: String,
+    limit: {
+      type: Number,
+      default: 0,
+    },
+  },
+  components: { ProductItem },
+  setup(props) {
+    let products = ref([]);
+
+    const getProduct = (category) => {
+      axios
+        .get(
+          `https://fakestoreapi.com/products/category/${category}?limit=${props.limit}`
+        )
+        .then((result) => {
+          products.value = [...result.data];
+        });
+    };
+
+    onMounted(() => {
+      getProduct(props.category);
+    });
+    watch(
+      () => props.category,
+      (newValue, oldValue) => {
+        getProduct(newValue);
+      }
+    );
+
+    return {
+      products,
+    };
   },
 };
+
+const getProducts = async () => {};
 </script>
 
 <template>
-  <div class="container">
-    <h2>{{ product.category }}</h2>
-    <div class="product-list">
-      <a v-for="item in product.data" :key="item.id" class="product-item">
-        <figure class="product-card-wrap-image">
-          <img :src="item.image" class="product-card-image" />
-        </figure>
-        <div class="product-card-description">
-          <span>{{ item.title }}</span>
-          <p>${{ item.price }}</p>
-        </div>
-      </a>
-      <!-- </div> -->
-    </div>
+  <h2>{{ category }}</h2>
+  <div class="flex">
+    <product-item
+      v-for="product in products"
+      :key="product.id"
+      v-bind:product="product"
+    ></product-item>
   </div>
 </template>
 
 <style scoped>
-h2 {
-  margin-bottom: 32px;
-  color: #fff;
-}
-.container {
-  max-width: 1360px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  top: 62px;
-  padding: 48px 8px 32px;
-  margin: 0 auto;
-}
-.product-card-wrap-image {
-  width: 100%;
-  height: 100%;
+.flex {
   display: flex;
   align-items: center;
-  justify-content: center;
-  background-color: #fff;
-}
-.product-card-image {
-  width: 100px;
-  height: 100px;
-}
-.product-item {
-  max-width: 316px;
-  width: 316px;
-  height: 468px;
-  overflow: hidden;
-  border-radius: 16px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-}
-.product-card {
-}
-.product-list {
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-.product-card-description {
-  width: 100%;
-  min-height: 180px;
-  color: #fff;
-  background-color: #394150;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  padding: 32px;
-}
-
-.product-card-description span {
-  margin-bottom: 20px;
 }
 </style>
