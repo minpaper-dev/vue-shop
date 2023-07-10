570d5edd136ea3e18a3acb0c0507758b0dfe615c
minji-yun <minpaper.dev@gmail.com>
2023-07-10 18:48:58 +0900

    [feat] router 등록

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 // import './style.css'
+import router from './router.js'
 import App from './App.vue'
 
+const app = createApp(App)
 
+app.use(router)
 
-
-createApp(App).mount('#app')
+app.mount("#app")
+// createApp(App).mount('#app')
