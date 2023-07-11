15dc87473f55058945a407534a8c36c0780cb9ad
minji-yun <minpaper.dev@gmail.com>
2023-07-11 20:56:07 +0900

    [feat] 404Page

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,38 @@
+<script>
+import { useRouter } from "vue-router";
+
+export default {
+  computed: {
+    fontColor() {
+      return "var(--primary-text)";
+    },
+  },
+  setup() {
+    const router = useRouter();
+    const goToMain = () => {
+      router.push({
+        name: "Main",
+      });
+    };
+
+    return {
+      goToMain,
+    };
+  },
+};
+</script>
+
+<template>
+  <div
+    :style="{ color: fontColor }"
+    class="wrap flex align-center direction-column"
+  >
+    <h2>404</h2>
+    <p>페이지를 찾을 수 없습니다.</p>
+    <button @click="goToMain">메인으로</button>
+  </div>
+</template>
+
+<style scoped>
+@import url(../assets/styles/main.css);
+</style>
--- src/router.js
+++ src/router.js
@@ -2,17 +2,20 @@ import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router
 import App from './App.vue'
 import Main from './components/MainPage.vue'
 import CategoryListPage from './components/CategoryListPage.vue'
-import Jewelery from './components/JeweleryPage.vue'
+import ProductDetail from './components/ProductDetail.vue'
+import CartPage from './components/CartPage.vue'
+import ErrorPage from './components/ErrorPage.vue'
 
 const routes = [
     // { path: '/', name : "Main", component: Main },
-    { path: '/',  component: Main },
+    { path: '/', name : "Main",  component: Main },
     { path: '/jewelery', name:"jewelery", component: CategoryListPage, props : {category :'jewelery' } },
     { path: '/electronics', name:"electronics",component: CategoryListPage , props : {category :'electronics' }},
     { path: '/men',name:"men", component: CategoryListPage , props : {category :"men's clothing" }},
     { path: '/women', name:"women",component: CategoryListPage, props : {category :"women's clothing" } },
-    { path: '/product/:id', component: Jewelery , props : true},
-    { path: "/:pathMatch(.*)", name : 'No-match', redirect : '/' },
+    { path: '/product/:id', component: ProductDetail , props : true},
+    { path: '/cart',name:"cart", component: CartPage },
+    { path: "/:pathMatch(.*)", name : 'No-match', component : ErrorPage },
   ]
 
  const router = createRouter({
