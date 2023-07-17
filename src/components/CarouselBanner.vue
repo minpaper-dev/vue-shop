d29cc1bd794c8816b5c0729a580714c4dd09aa0e
minji-yun <minpaper.dev@gmail.com>
2023-07-17 22:30:44 +0900

    [style] tailwind 적용

--- src/components/CarouselBanner.vue
+++ src/components/CarouselBanner.vue
@@ -51,10 +51,14 @@ export default {
             backgroundPosition: 'center',
           }"
         >
-          <div class="banner-text">
-            <h2>{{ bannerItem.title }}</h2>
-            <p>{{ bannerItem.description }}</p>
-            <button>바로가기</button>
+          <div class="banner-text text-white">
+            <h2 class="text-4xl">{{ bannerItem.title }}</h2>
+            <p class="text-base">{{ bannerItem.description }}</p>
+            <button
+              class="bg-black dark:bg-gray-400 text-gray-600 dark:text-white"
+            >
+              바로가기
+            </button>
           </div>
         </div>
       </slide>
@@ -86,6 +90,5 @@ export default {
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
-  color: #fff;
 }
 </style>
