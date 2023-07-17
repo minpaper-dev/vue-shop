2765ee441ddf509547d69a74b96e647218e5642f
minji-yun <minpaper.dev@gmail.com>
2023-07-17 14:44:36 +0900

    [etc] tailwind 적용

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
-// import './style.css'
+import './index.css'
+
 import router from './router.js'
 import App from './App.vue'
+import store from './store.js'
 
 const app = createApp(App)
 
+app.use(store)
 app.use(router)
 
 app.mount("#app")
